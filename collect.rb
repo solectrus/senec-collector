843efd58cd0c7535ed05c0f5d3f08e8a9c878753
Georg Ledermann <georg@ledermann.dev>
2020-12-26 09:23:00 +0100

    Improve logging

--- collect.rb
+++ collect.rb
@@ -2,15 +2,21 @@ require 'dotenv/load'
 require 'senec'
 require 'influxdb-client'
 
-puts 'Starting SENEC collector...'
+# Flush output immediately
+$stdout.sync = true
 
+puts "Starting SENEC collector...\n\n"
+
+print 'Checking ENV variables ... '
 senec_host    = ENV.fetch('SENEC_HOST')
 influx_host   = ENV.fetch('INFLUX_HOST')
 influx_token  = ENV.fetch('INFLUX_TOKEN')
 influx_org    = ENV.fetch('INFLUX_ORG')
 influx_bucket = ENV.fetch('INFLUX_BUCKET')
+puts 'OK'
 
 # Setup InfluxDB
+print "Connecting to InfluxDB at #{influx_host} ... "
 client = InfluxDB2::Client.new(
   influx_host,
   influx_token,
@@ -18,24 +24,32 @@ client = InfluxDB2::Client.new(
 )
 write_api = client.create_write_api
 
+puts 'OK'
+puts "Starting loop.\n\n"
+
+count = 0
 loop do
+  count += 1
+  puts "\n-------------------------------------------------------\n"
+
   # Get data from SENEC
+  print "##{count}: Getting data from SENEC at #{senec_host} ... "
   request = Senec::Request.new host: senec_host
+  puts 'OK'
 
-  puts `clear`
-
-  puts "PV production: #{request.inverter_power} W"
-  puts "House power consumption: #{request.house_power} W"
+  puts "\n"
+  puts "  PV production: #{request.inverter_power} W"
+  puts "  House power consumption: #{request.house_power} W"
   puts "\n"
 
-  puts "Battery charge power: #{request.bat_power} W"
-  puts "Battery fuel charge: #{request.bat_fuel_charge} %"
-  puts "Battery charge current: #{request.bat_charge_current} A"
-  puts "Battery voltage: #{request.bat_voltage} V"
+  puts "  Battery charge power: #{request.bat_power} W"
+  puts "  Battery fuel charge: #{request.bat_fuel_charge} %"
+  puts "  Battery charge current: #{request.bat_charge_current} A"
+  puts "  Battery voltage: #{request.bat_voltage} V"
   puts "\n"
 
-  puts "Grid power: #{request.grid_power} W"
-  puts "Current state of the system: #{request.current_state}"
+  puts "  Grid power: #{request.grid_power} W"
+  puts "  Current state of the system: #{request.current_state}"
   puts "\n"
 
   # Push data to InfluxDB
@@ -50,10 +64,11 @@ loop do
                           .add_field('grid_power_plus',    request.grid_power.positive? ? request.grid_power : 0)
                           .add_field('grid_power_minus',   request.grid_power.negative? ? -request.grid_power : 0)
 
+  print 'Pushing to InfluxDB ... '
   write_api.write(data: point, bucket: influx_bucket, org: influx_org)
-
-  puts 'Successfully pushed to InfluxDB.'
+  puts 'OK'
 
   # Wait
+  puts 'Sleeping ...'
   sleep 5
 end

df5d8a40f5934b243b30a1efc1cb35af063a8dd5
Georg Ledermann <georg@ledermann.dev>
2021-01-13 13:20:03 +0100

    Get and push wallbox_charge_power

--- collect.rb
+++ collect.rb
@@ -49,21 +49,25 @@ loop do
   puts "  Battery voltage: #{request.bat_voltage} V"
   puts "\n"
 
+  puts "  Wallbox charge power: #{request.wallbox_charge_power.sum} W"
+  puts "\n"
+
   puts "  Grid power: #{request.grid_power} W"
   puts "  Current state of the system: #{request.current_state}"
   puts "\n"
 
   # Push data to InfluxDB
   point = InfluxDB2::Point.new(name: 'SENEC')
-                          .add_field('inverter_power',     request.inverter_power)
-                          .add_field('house_power',        request.house_power)
-                          .add_field('bat_power_plus',     request.bat_power.positive? ? request.bat_power : 0)
-                          .add_field('bat_power_minus',    request.bat_power.negative? ? -request.bat_power : 0)
-                          .add_field('bat_fuel_charge',    request.bat_fuel_charge)
-                          .add_field('bat_charge_current', request.bat_charge_current)
-                          .add_field('bat_voltage',        request.bat_voltage)
-                          .add_field('grid_power_plus',    request.grid_power.positive? ? request.grid_power : 0)
-                          .add_field('grid_power_minus',   request.grid_power.negative? ? -request.grid_power : 0)
+                          .add_field('inverter_power',       request.inverter_power)
+                          .add_field('house_power',          request.house_power)
+                          .add_field('bat_power_plus',       request.bat_power.positive? ? request.bat_power : 0)
+                          .add_field('bat_power_minus',      request.bat_power.negative? ? -request.bat_power : 0)
+                          .add_field('bat_fuel_charge',      request.bat_fuel_charge)
+                          .add_field('bat_charge_current',   request.bat_charge_current)
+                          .add_field('bat_voltage',          request.bat_voltage)
+                          .add_field('wallbox_charge_power', request.wallbox_charge_power.sum)
+                          .add_field('grid_power_plus',      request.grid_power.positive? ? request.grid_power : 0)
+                          .add_field('grid_power_minus',     request.grid_power.negative? ? -request.grid_power : 0)
 
   print 'Pushing to InfluxDB ... '
   write_api.write(data: point, bucket: influx_bucket, org: influx_org)

fa969fda4d7edf004a59dcda701141ee91010c2a
Georg Ledermann <georg@ledermann.dev>
2023-09-08 15:40:26 +0200

    Add support for language customisation

--- app/config.rb
+++ app/config.rb
@@ -3,6 +3,7 @@ Config =
     :senec_host,
     :senec_schema,
     :senec_interval,
+    :senec_language,
     :influx_schema,
     :influx_host,
     :influx_port,
@@ -27,8 +28,11 @@ Config =
     def senec_state_names
       @senec_state_names ||=
         begin
-          puts 'Getting state names from SENEC by parsing source code...'
-          names = Senec::State.new(connection: senec_connection).names
+          puts "Getting state names (language: #{senec_language}) from SENEC by parsing source code..."
+          names =
+            Senec::State.new(connection: senec_connection).names(
+              language: senec_language,
+            )
           puts "OK, got #{names.length} state names"
           names
         end
@@ -64,6 +68,7 @@ Config =
           senec_host: ENV.fetch('SENEC_HOST'),
           senec_schema: ENV.fetch('SENEC_SCHEMA', 'http'),
           senec_interval: ENV.fetch('SENEC_INTERVAL').to_i,
+          senec_language: ENV.fetch('SENEC_LANGUAGE', 'de').to_sym,
           influx_host: ENV.fetch('INFLUX_HOST'),
           influx_schema: ENV.fetch('INFLUX_SCHEMA', 'http'),
           influx_port: ENV.fetch('INFLUX_PORT', '8086'),

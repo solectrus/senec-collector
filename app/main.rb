4d583834a2b11f8dbaefc6c73d7ecf2dbcb7ac49
Georg Ledermann <georg@ledermann.dev>
2024-01-11 08:35:11 +0100

    2024

--- app/main.rb
+++ app/main.rb
@@ -11,7 +11,7 @@ puts 'SENEC collector for SOLECTRUS, ' \
        "Version #{ENV.fetch('VERSION', '<unknown>')}, " \
        "built at #{ENV.fetch('BUILDTIME', '<unknown>')}"
 puts 'https://github.com/solectrus/senec-collector'
-puts 'Copyright (c) 2020,2023 Georg Ledermann, released under the MIT License'
+puts 'Copyright (c) 2020-2024 Georg Ledermann, released under the MIT License'
 puts "\n"
 
 config = Config.from_env
